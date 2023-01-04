184f25dc315c777ceecf7814632ff01a571d8c65
pedroobando <pedroobando@hotmail.com>
2023-01-03 21:38:55 -0400

    update

--- src/store/slices/post/postsSlice.ts
+++ src/store/slices/post/postsSlice.ts
@@ -19,36 +19,6 @@ const initialState: IPostState = {
   error: null,
 };
 
-// initialState.status;
-// const initialState: IPOST[] = [
-//   {
-//     id: '1',
-//     title: 'Learning Redux Toolkit',
-//     content: "I've heard good things.",
-//     date: sub(new Date(), { minutes: 10 }).toISOString(),
-//     reactions: {
-//       thumbsUp: 0,
-//       wow: 0,
-//       heart: 0,
-//       rocket: 0,
-//       coffee: 0,
-//     },
-//   },
-//   {
-//     id: '2',
-//     title: 'Slices...',
-//     content: 'The more I say slice, the more I want pizza.',
-//     date: sub(new Date(), { minutes: 5 }).toISOString(),
-//     reactions: {
-//       thumbsUp: 0,
-//       wow: 0,
-//       heart: 0,
-//       rocket: 0,
-//       coffee: 0,
-//     },
-//   },
-// ];
-
 export const postsSlice = createSlice({
   name: 'posts',
   initialState,
@@ -140,7 +110,7 @@ export const postsSlice = createSlice({
           rocket: 0,
           coffee: 0,
         };
-        // console.log(addNewPost);
+
         state.posts.push(addNewPost);
       });
   },
